42b7ea1fc7518046283195061be9996e9773cade
navilaael <navilaael@gmail.com>
2025-02-15 10:36:14 +0700

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 
 const login = () => {
   return (
-    <div>login</div>
+    <div className='flex justify-center items-center'><Card>
+    <CardHeader>
+      <CardTitle>Card Title</CardTitle>
+      <CardDescription>Card Description</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <p>Card Content</p>
+    </CardContent>
+    <CardFooter>
+      <p>Card Footer</p>
+    </CardFooter>
+  </Card>
+  </div>
   )
 }
 
